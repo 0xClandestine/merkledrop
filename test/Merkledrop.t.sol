70be89ae8c9e01108c08af84ba9c804927f7c5e4
0xClandestine <clandestine.eth@proton.me>
2022-11-22 20:12:30 -0600

    ♻️more robust tests

--- test/Merkledrop.t.sol
+++ test/Merkledrop.t.sol
@@ -27,19 +27,40 @@ contract MerkledropTest is Test {
         token = new MockERC20();
     }
 
-    function testMerkledrop() public {
-        address alice = address(0xAAAA);
-        address bob = address(0xBBBB);
+    function getAddress(bytes32 salt, uint256 index)
+        internal
+        pure
+        returns (address)
+    {
+        return address(uint160(uint256(keccak256(abi.encode(salt, index)))));
+    }
 
-        uint256 aliceAmount = 420 ether;
-        uint256 bobAmount = 69 ether;
-        uint256 totalAirdrop = aliceAmount + bobAmount;
+    function getAmount(bytes32 salt, uint256 index)
+        internal
+        pure
+        returns (uint256)
+    {
+        return uint256(keccak256(abi.encode(salt, index))) % 100_000 ether;
+    }
+
+    function testMerkledrop(bytes32 salt, uint256 dataLen) public {
+        vm.assume(dataLen < 100 && dataLen > 5);
 
         // Define contents of merkle tree.
-        bytes32[] memory data = new bytes32[](2);
+        bytes32[] memory data = new bytes32[](dataLen);
 
-        data[0] = keccak256(abi.encodePacked(alice, uint256(aliceAmount)));
-        data[1] = keccak256(abi.encodePacked(bob, uint256(bobAmount)));
+        uint256 totalAirdrop;
+
+        unchecked {
+            for (uint256 i; i < dataLen; ++i) {
+                uint256 amount = getAmount(salt, i);
+
+                data[i] =
+                    keccak256(abi.encodePacked(getAddress(salt, i), amount));
+
+                totalAirdrop += amount;
+            }
+        }
 
         // Caclulate merkle root.
         bytes32 root = murky.getRoot(data);
@@ -56,91 +77,99 @@ contract MerkledropTest is Test {
         assertEq(merkledrop.asset(), address(token));
         assertEq(merkledrop.merkleRoot(), root);
 
-        // Create proofs
-        bytes32[] memory aliceProof = murky.getProof(data, 0);
-        bytes32[] memory bobProof = murky.getProof(data, 1);
+        // Generate proofs
+        bytes32[][] memory proofs = new bytes32[][](dataLen);
 
-        // Ensure users cannot claim more than intended.
-        vm.prank(alice);
-        vm.expectRevert();
-        merkledrop.claim(aliceProof, aliceAmount + 1);
+        unchecked {
+            for (uint256 i; i < dataLen; ++i) {
+                proofs[i] = murky.getProof(data, i);
 
-        // Ensure users cannot claim less than intended.
-        vm.prank(alice);
-        vm.expectRevert();
-        merkledrop.claim(aliceProof, aliceAmount - 1);
+                // Ensure address cannot claim more than intended.
+                vm.prank(getAddress(salt, i));
+                vm.expectRevert();
+                merkledrop.claim(proofs[i], getAmount(salt, i) + 1);
 
-        // Ensure alice can claim as expected.
-        vm.prank(alice);
-        merkledrop.claim(aliceProof, aliceAmount);
+                // Ensure address cannot claim less than intended.
+                vm.prank(getAddress(salt, i));
+                vm.expectRevert();
+                merkledrop.claim(proofs[i], getAmount(salt, i) - 1);
 
-        // Ensure bob can claim as expected.
-        vm.prank(bob);
-        merkledrop.claim(bobProof, bobAmount);
+                // Ensure address can claim as expected.
+                vm.prank(getAddress(salt, i));
+                merkledrop.claim(proofs[i], getAmount(salt, i));
 
-        // Ensure users cannot replay their claim.
-        vm.prank(bob);
-        vm.expectRevert();
-        merkledrop.claim(bobProof, bobAmount);
+                // Ensure address cannot replay claim.
+                vm.prank(getAddress(salt, i));
+                vm.expectRevert();
+                merkledrop.claim(proofs[i], getAmount(salt, i));
+            }
+        }
     }
 
-    function testMerkledropNative() public {
-        address alice = address(0xAAAA);
-        address bob = address(0xBBBB);
-
-        uint256 aliceAmount = 420 ether;
-        uint256 bobAmount = 69 ether;
-        uint256 totalAirdrop = aliceAmount + bobAmount;
+    function testMerkledropNative(bytes32 salt, uint256 dataLen) public {
+        vm.assume(dataLen < 100 && dataLen > 5);
 
         // Define contents of merkle tree.
-        bytes32[] memory data = new bytes32[](2);
+        bytes32[] memory data = new bytes32[](dataLen);
 
-        data[0] = keccak256(abi.encodePacked(alice, uint256(aliceAmount)));
-        data[1] = keccak256(abi.encodePacked(bob, uint256(bobAmount)));
+        uint256 totalAirdrop;
+
+        unchecked {
+            for (uint256 i; i < dataLen; ++i) {
+                uint256 amount = getAmount(salt, i);
+
+                data[i] =
+                    keccak256(abi.encodePacked(getAddress(salt, i), amount));
+
+                totalAirdrop += amount;
+            }
+        }
 
         // Caclulate merkle root.
         bytes32 root = murky.getRoot(data);
 
-        // Give this contract some test ether to create a merkledrop.
-        deal(address(this), totalAirdrop);
+        // Give this contract some test tokens to create a merkledrop.
+        token.mint(address(this), totalAirdrop);
+        token.approve(address(factory), totalAirdrop);
         merkledrop = Merkledrop(
             payable(
                 factory.create{value: totalAirdrop}(
-                    address(0), root, totalAirdrop
+                    address(token), root, totalAirdrop
                 )
             )
         );
 
         // Ensure immutables were setup properly.
         assertEq(merkledrop.creator(), address(this));
-        assertEq(merkledrop.asset(), address(0));
+        assertEq(merkledrop.asset(), address(token));
         assertEq(merkledrop.merkleRoot(), root);
 
-        // Create proofs
-        bytes32[] memory aliceProof = murky.getProof(data, 0);
-        bytes32[] memory bobProof = murky.getProof(data, 1);
+        // Generate proofs
+        bytes32[][] memory proofs = new bytes32[][](dataLen);
 
-        // Ensure users cannot claim more than intended.
-        vm.prank(alice);
-        vm.expectRevert();
-        merkledrop.claim(aliceProof, aliceAmount + 1);
+        unchecked {
+            for (uint256 i; i < dataLen; ++i) {
+                proofs[i] = murky.getProof(data, i);
 
-        // Ensure users cannot claim less than intended.
-        vm.prank(alice);
-        vm.expectRevert();
-        merkledrop.claim(aliceProof, aliceAmount - 1);
+                // Ensure address cannot claim more than intended.
+                vm.prank(getAddress(salt, i));
+                vm.expectRevert();
+                merkledrop.claim(proofs[i], getAmount(salt, i) + 1);
 
-        // Ensure alice can claim as expected.
-        vm.prank(alice);
-        merkledrop.claim(aliceProof, aliceAmount);
+                // Ensure address cannot claim less than intended.
+                vm.prank(getAddress(salt, i));
+                vm.expectRevert();
+                merkledrop.claim(proofs[i], getAmount(salt, i) - 1);
 
-        // Ensure bob can claim as expected.
-        vm.prank(bob);
-        merkledrop.claim(bobProof, bobAmount);
+                // Ensure address can claim as expected.
+                vm.prank(getAddress(salt, i));
+                merkledrop.claim(proofs[i], getAmount(salt, i));
 
-        // Ensure users cannot replay their claim.
-        vm.prank(bob);
-        vm.expectRevert();
-        merkledrop.claim(bobProof, bobAmount);
+                // Ensure address cannot replay claim.
+                vm.prank(getAddress(salt, i));
+                vm.expectRevert();
+                merkledrop.claim(proofs[i], getAmount(salt, i));
+            }
+        }
     }
 }
